f45d89b181f0eb428588b58221a97536f3c448af
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-18 18:05:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,13 @@ def long_planeteer_calls(array)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array_of_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array_of_strings.each do |string|
+    if cheese_types.include?(string)
+      return string
+    end 
+  end 
+  return nil
 end
